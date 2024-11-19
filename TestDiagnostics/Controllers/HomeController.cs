f8f21d21b109bcf054c560345977903011c8b99d
Lucas Sanches <3931027+lcssanches@users.noreply.github.com>
2024-11-19 19:50:18 +0100

    Add initial project structure and configuration files

---
+++ TestDiagnostics/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Web.Http;
+
+namespace TestDiagnostics.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet()]
+        public object Index()
+        {
+            //var a = new ContentResult()
+            //{
+            //    Content = System.Text.Json.JsonSerializer.Serialize(new
+            //    {
+            //        CurrentDirectory = AppContext.BaseDirectory,
+            //        DiagnosticFileExists = System.IO.File.Exists("OTEL_DIAGNOSTICS.json")
+            //    }),
+            //    ContentType = "application/json"
+            //};
+
+            return Json(new
+            {
+                AppContext_BaseDirectory = AppContext.BaseDirectory,
+                AppContext_BaseDirectory_FileExists = File.Exists(Path.Combine(AppContext.BaseDirectory,"OTEL_DIAGNOSTICS.json")),
+                Directory_GetCurrentDirectory = Directory.GetCurrentDirectory(),
+                Directory_GetCurrentDirectory_FileExists = File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "OTEL_DIAGNOSTICS.json")),
+                FileExists = File.Exists("OTEL_DIAGNOSTICS.json"),
+            });
+        }
+    }
+}
---
+++ TestDiagnostics/Global.asax.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using System.Configuration;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TestDiagnostics
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        private ILoggerFactory loggerFactory;
+        private TracerProvider? tracerProvider;
+        private MeterProvider? meterProvider;
+        protected void Application_Start()
+        {
+            StartTelemetry();
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void StartTelemetry()
+        {
+            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["APPLICATIONINSIGHTS_CONNECTION_STRING"])) return;
+
+
+            loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddOpenTelemetry(logging =>
+                {
+                    logging.AddConsoleExporter();
+                });
+            });
+
+
+            tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddAspNetInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddSqlClientInstrumentation(x =>
+                {
+                    x.SetDbStatementForStoredProcedure = true;
+                    x.SetDbStatementForText = true;
+                }).AddConsoleExporter()
+                .Build();
+            
+            meterProvider = Sdk.CreateMeterProviderBuilder()
+                .AddAspNetInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddConsoleExporter()
+                .Build();
+        }
+
+    }
+}
